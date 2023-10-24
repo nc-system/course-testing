157a7f929f3fdf485dbd3fac1d100309d6002275
Andres Giraldo <andresganc@gmail.com>
2023-10-24 09:21:26 -0500

    Mod: Test Sum & Sub

--- javascript/jest/examples/examples-test-jest-js/src/01-operations/01-sum.test.js
+++ javascript/jest/examples/examples-test-jest-js/src/01-operations/01-sum.test.js
@@ -1,6 +1,23 @@
 
 const sum = require('./01-sum')
 
-test('Adition 2 + 1 = 3', () => {
-  expect(sum(2, 1)).toBe(3)
+
+describe('Addition Test', () => {
+  
+  test('Adition 2 + 1 = 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  it('Should return 3 whit 1 + 2', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  test('Should 1 + (-2) is -1', () => {
+    expect(sum(1, -2)).toBe(-1)
+  });
+
+  test('Should return -3, The addition -1 + (-2)', () => {
+    expect(sum(-1, -2)).toBe(-3)
+  });
+
 })
--- javascript/jest/examples/examples-test-jest-js/src/01-operations/02-sub.test.js
+++ javascript/jest/examples/examples-test-jest-js/src/01-operations/02-sub.test.js
@@ -1,6 +1,14 @@
 
 const sub = require('./02-sub')
 
-test('Substration 2 - 1 = 1', () => {
-    expect(sub(2 - 1)).toBe(1)
-})
+describe('Substraction Test', () => {
+
+    test('Substration 2 - 1 = 1', () => {
+        expect(sub(2, 1)).toBe(1)
+    })
+
+    it('Should return 1 whit 2 - 1', () => {
+        expect(sub(2, 1)).toBe(1)    
+    });
+});
+
